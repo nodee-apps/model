3c258d6c9396a3c8718120f886ac669f86af079d
Matus Szabo <matus.szabo@gmail.com>
2016-02-26 00:13:52 +0100

    fixed: tree behaviour validation

--- test/behaviours/tree.js
+++ test/behaviours/tree.js
@@ -91,7 +91,7 @@ function testHooks(cb){
                 
                 records[2].ancestors = ['0','2']; // will return error, because ancestor with id '0' not exists
                 PersonTree.new( records[2] ).create(function(err, record3){
-                    assert.ok( err.message === 'Tree.prototype.validateAncestors: cannot find all ancestors' );
+                    assert.ok( err.message === 'PersonTree.prototype.validateAncestors: cannot find all ancestors' );
                     
                     records[2].ancestors = ['1','2'];
                     PersonTree.new( records[2] ).create(function(err, record3){
@@ -130,7 +130,7 @@ function testHooks(cb){
                     
                     records[3].ancestors = ['1','2','3']; // test inconsistent ancestors, correctly have to be ['1','3']
                     PersonTree.new( records[3] ).create(function(err, record4){
-                        assert.ok(err.message === 'Tree.prototype.validateAncestors: inconsistent path');
+                        assert.ok(err.message === 'PersonTree.prototype.validateAncestors: inconsistent path');
                         
                         records[3].ancestors = ['1','3'];
                         PersonTree.new( records[3] ).create(function(err, record4){
