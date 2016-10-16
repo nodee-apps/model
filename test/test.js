66b3c5a5dae2d37033b30d461c33d24c883c992b
Matus Szabo <matus.szabo@gmail.com>
2016-10-16 18:35:02 +0200

    updated: elastic search version 2.4 support

--- test/test.js
+++ test/test.js
@@ -21,5 +21,5 @@ require('./datasources/fileSystemDS.js');
 // test require connection to mongodb, please use your own databse
 require('./datasources/mongoDS.js')('ds061365.mongolab.com', 61365, 'test', 'pass123');
 
-// test require connection to elasticSearch, please use your own databse
-require('./datasources/elasticSearchDS.js')('https://VvpJw9DvREeSN1HMjNvV4AY9TrMYjYaE:@nodeetest.east-us.azr.facetflow.io');
+// test require connection to elasticSearch, please use your own elasticsearch
+require('./datasources/elasticSearchDS.js')('https://gf9b8nkuvr:rjvhm2jhiy@nodee-2480254251.eu-west-1.bonsai.io');
