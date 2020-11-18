c46bf06a496c4b0564f22f4ed2a12e4625d56565
Matus Szabo <matus.szabo@gmail.com>
2020-11-18 16:09:39 +0100

    added: mongo data source aggregate method, fixed conn string parsing typo

--- test/test.js
+++ test/test.js
@@ -21,5 +21,6 @@ require('./datasources/fileSystemDS.js');
 // test require connection to mongodb, please use your own databse
 require('./datasources/mongoDS.js')(null, 'ds061365.mongolab.com', 61365, 'test', 'pass123');
 
+// TODO: update elastic search new version compatibility
 // test require connection to elasticSearch, please use your own elasticsearch
-require('./datasources/elasticSearchDS.js')('https://n9klpghqtw:c3n7jf7908@nodee-1012018453.eu-central-1.bonsaisearch.net');
+// require('./datasources/elasticSearchDS.js')('https://oi6juoqj4w:bhzfvm8a08@nodee-test-9340157225.us-east-1.bonsaisearch.net:443');
